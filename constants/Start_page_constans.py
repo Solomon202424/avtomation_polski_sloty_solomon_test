bf612f1d55e96165852c48f0dc21cdd70438d8fd
solomon <solomon.1987.2020@gmail.com>
2023-09-18 23:59:08 +0300

    Files

--- constants/Start_page_constans.py
+++ constants/Start_page_constans.py
@@ -3,6 +3,25 @@ class StartPageConstans:
     url = "https://polskiesloty.com/"
     h1_xpath = ".//h1"
     h2_xpath_text = ".//h2[text()='Jak Wybrać Najlepsze Polskie Sloty?']"
+    polski_sloti_logo_xpath= ".//img[@class='no-lazy' and @alt='polski sloty logo']"
+    bodi_xpath = ".//body"
+    butom_skrol_up_xpath = ".//div[@class='scroll is_scrolledBox']"
+    ref_botom_icecasino_xpath = ".//a[@href='/goto/icecasino/']"
+    redirect_url_xpath = "my-promo7.com/ice_50fs"
+    butom_to_kasyna_xpath = "//a[contains(.,'Lista Online Kasyn')]"
+    page_kasyna_h2_xpath = "//h2[@class='h2'][contains(.,'Lista Polskich Kasyn Online')]"
+    butom_to_sloti_xpath = "//a[contains(.,'GG Bet Opinie')]"
+    butom_search_xpath = "//input[contains(@id,'search-input')]"
+    search_send_keys_text_xpath = "Book of"
+    page_search_slot_BookofRa10_xpath = "//div[@class='title-slots'][contains(.,'Book of Ra 10')]"
+    page_sloti_text_xpath = "GG Bet Opinie – Recenzja Kasyna Online"
+
+
+
+
+
+
+
 
 
 
